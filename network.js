37cfa967adba6775ca882ffbe64583f142be75d3
itbergl <tberg644@gmail.com>
2021-07-07 20:00:05 +0800

    input training images

--- network.js
+++ network.js
@@ -7,6 +7,8 @@ class Network {
         this.layers = [];
 
         this.alpha = 0.5;
+        this.target_buffer = null;
+        this.input_buffer = null;
     }
 
     addHiddenLayer(dim) {
@@ -30,6 +32,29 @@ class Network {
         console.log(this.output_layer.desc);
     }
 
+    inputTrainingImage(image_index) {
+
+        // get image and target
+        let image_map = data_set[image_index];
+        let target_num = parseInt(Object.keys(image_map));
+
+        // construct zero array with a 1 in the target position
+        let prim_array = []
+        for (let i = 0; i < 10; i++) {
+            if (i != target_num) {
+                prim_array.push(0);
+            }
+            else {
+                prim_array.push(1);
+            }
+        }
+        //convert to math.js matrix object for processing
+        let target_vec = math.matrix([prim_array]);
+
+        // hold the input data and real target in the network class
+        this.target_buffer = target_vec;
+        this.input_buffer = image_map[target_num];
+    }
 }
 
 class Layer {
@@ -88,6 +113,23 @@ class OutputLayer extends InnerLayer {
     get desc() {
         return `Output Layer (${this.dim} nodes)`;
     }
+
+    processOutput(input) {
+        let out_vec = math.multiply(this.weight, inputVec) + this.bias;
+
+
+        //get softmax vector
+
+        //calculate loss
+
+
+
+    }
+
+    softmax() {
+
+    }
+
 }
 
 let network = new Network(2048, 10);
--- run.js
+++ run.js
@@ -7,5 +7,15 @@ network.addHiddenLayer(100);
 console.log(network.desc)
 
 
-console.log(imageData[0]);
+// console.log(imageData[0]);
+
+// let dodo = [{ "5": [0, 0, 0] }, { "6": [1, 1, 1] }, { "4": [2, 2, 2] }]
+
+// console.log(Object.keys(dodo[0]));
+
+network.inputTrainingImage(0)
+
+console.log(network.input_buffer);
+console.log(network.target_buffer);
+
 // console.log("100");
